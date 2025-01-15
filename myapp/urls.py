0850acab2d3bbdde3d6236599f7e860f2fcf01dc
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-15 11:23:28 -0400

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.index, name="index"),
+    path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('hello/<str:username>', views.hello, name="hello"),
     path('projects/', views.projects, name="projects"),
     path('tasks/', views.tasks, name="tasks"),
     path('create_task/', views.create_task, name="create_task"),
     path('create_project/', views.create_project, name="create_project"),
+    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -55,3 +55,8 @@ def create_project(request):
     else:
         Project.objects.create(name=request.POST['name'])
         return redirect('projects')
+    
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('tasks')
